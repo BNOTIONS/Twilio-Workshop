88b4e79d2cea81fc88806daabd153e2a9984fe05
Lowgain <logan@bnotions.com>
2012-10-13 13:15:00 -0400

    annotated the phoneroulette example

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Twilio Workshop
 
-Awesome!!!!
+## Awesome!!!!
+Currently this contains the phoneroulette sample project we went over in the workshop.
+I'll get Naeem's PHP examples from him as well, and perhaps a choose your own adventure game I never got to show.
+
+## Setup
+If you want to run this example on your machine, you'll want the following:
+- Python with the flask and twilio libraries (pip install flask twilio)
+- A Twilio account and a phone number
+- Localtunnel to let people use your app publicly from your machine (gem install localtunnel) (note: not suitable for production)
+
+After you get those setup, you use a twilio phone number, and point the call action to GET http://xxxx.localtunnel.com/phonecall
--- phoneroulette.py
+++ phoneroulette.py
@@ -1,16 +1,29 @@
+"""
+Phoneroulette - Super tiny webapp that you can use to call
+a random friend!
+Last Updated 10.13.12 by Logan
+"""
 from flask import Flask, request, render_template
 from twilio import twiml
 from random import choice
 
+# This number should be the number you hooked up on twilio
 HOTLINE_NUMBER = '911'
 
+# In a production app you'd want some sort of db, in-memory is cool for now
 phone_numbers = []
 
+# And here is our actual magic Flask app!
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """
+    This is the main index page. It displays a number submission form,
+    and adds the number to the list if the form submits.
+    In a production application, we'd want some validation/deduplication!
+    """
     if 'number' in request.form:
         phone_numbers.append(request.form['number'])
         return "Cool Thanks!!!!"
@@ -20,16 +33,27 @@ def index():
 
 @app.route('/nums')
 def numbers():
+    """
+    This is just a debug path that lists all the numbers we have in memory.
+    A production app should probably not have this turned on!
+    """
     return '<pre>' + '\n'.join(phone_numbers) + '</pre>'
 
 
 @app.route('/phonecall')
 def phonecall():
+    """
+    This is the path that Twilio hits when someone calls your phone number.
+    It generates the TwiML that tells Twilio to call a number from the list.
+    A production app would make sure you didn't call yourself, and probably
+    retry if someone else had a busy signal.
+    """
     phone_number = choice(phone_numbers)
     r = twiml.Response()
     r.dial(phone_number)
     return str(r)
 
 
+# And this is where we run the App! Turning debug on lets us see errors
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
